dcf0278601210b8d45f78f62ac20e3ed4fd73a93
Anh La Russo <Aphuong85@gmail.com>
2014-11-18 12:03:12 -0500

    Add additional svg features and styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,14 @@ $(document).ready(function(){
   };
   bodyRound();
 
+  window.bodyPear = function(){
+    d3.select("svg").append("path")
+                    .attr("d", "M207.3 115.3c13.3 15.5 9.2 44.6 2.7 56 c-11.6 20.1-40.2 27.9-59.7 28c-27.3 0.1-65.5-11.1-66.2-59.7c-0.2-11.3 4.9-20.4 8.5-24.7c7.6-9 3.7-4 12.3-13.3 c0.4-0.4 4.3-2.7 8-12c3.3-8.3 10-40.7 39.3-40.7c32.7 0 27.2 17.9 39.7 46C194 99.4 202.3 109.4 207.3 115.3z")
+                    .attr("class", "body-pear");
+  };
+  // bodyPear();
+
+
   window.eyesOpen = function(){
     d3.select("svg").append("circle")
                     .attr("cx", 125.7)
@@ -139,7 +147,41 @@ $(document).ready(function(){
                     .attr("d", "M188.6 15.8c0 0-13.2-4.3-24.3 3.7s-11.6 25.8-11.6 25.8 s1.3-2.4 2.6-3.2s7.1-1 7.1-1s9 0.1 13.2-3.7s5.9-10.8 6.5-15.4S188.6 15.8 188.6 15.8z")
                     .attr("class", "hair-leaf")  
   };
-  hairLeaf();
+  // hairLeaf();
+
+
+  window.hairDoubleLeaf = function(){
+    d3.select("svg").append("g").append("path")
+                    .attr("d", "M153.2 32.1c0-1.2-1-2.1-2.1-2.1l0 0c-1.3 0-1.4 0.6-3.3 1.8 l3.2 20.5c-1.9 1.4-1 2.3 0.2 2.3l0 0c1.2 0 2.1-1 2.1-2.1V32.1z")
+                    .attr("class", "hair-double-leaf");
+    d3.select("svg").append("path")
+                    .attr("d", "M188.6 15.8c0 0-13.2-4.3-24.3 3.7s-11.6 25.8-11.6 25.8 s1.3-2.4 2.6-3.2s7.1-1 7.1-1s9 0.1 13.2-3.7s5.9-10.8 6.5-15.4S188.6 15.8 188.6 15.8z")
+                    .attr("class", "hair-double-leaf");
+    d3.select("svg").append("g").append("path")
+                    .attr("d", "M55.3-14")
+                    .attr("class", "hair-double-leaf");
+    d3.select("svg").append("path")
+                    .attr("d", "M148 37.7c-0.9-10.4-12.7-16.4-21.2-10.3 c-4.6 3.3-7 8.4-10.4 12.7c-1.6 2.1-5.6 6.1-6 8.6c3.3 0.2 7 0.8 10.3 0.4c4.3-0.5 8-2.6 11.7-4.5c6-3.1 8.8-7.2 15.9-7.5 c0.1-0.5 0.1-1.1 0.1-1.6")
+                    .attr("class", "hair-double-leaf");
+  }
+  // hairDoubleLeaf();
+
+
+  window.hairShaggy = function(){
+    d3.select("svg").append("g").append("g").append("path")
+                    .attr("d", "M151.8 48.5l1.9-0.2c7-0.8 16.7 2.4 20.6 8.6 c-4.8-2.6-10.4-1.2-15.5-1.3c5.2 2.3 14 3.1 15.2 10c-6.2-4-13-4.8-20.2-5c5.1 4.7 9.1 10.4 13.2 16c-7.5-1.5-13.7-9.9-19.7-14.2 C150 69 146 76 146.7 82.7c-4.3-7-6.7-14.9-10-22.3c-1.2 6.9-7.4 11.7-9 18.5c0-7.4-0.8-17 4.7-22.9")
+                    .attr("class", "hair-shaggy");
+    d3.select("svg").append("g").append("path")
+                    .attr("d", "M151.4 49.1c-0.6-4.7-0.7-9.8-2.8-14 c-2.2-4.4-7.3-2.5-11.3-1.7c-3.9 0.8-8.8 2.1-9.2 6.6c-0.4 3.9 2.7 8.1 4.6 11.4l-1 1.1c-5.6 1.6-10.2 8.3-12.3 13.4 c3.7-2.4 7.2-5.2 11-7.5l0.1 0.3")
+                    .attr("class", "hair-shaggy");
+    d3.select("svg").append("path")
+                    .attr("d", "M128.6 38c5.3 1.9 15.3 0 18-5.2")
+                    .attr("class", "hair-shaggy-outline");
+    d3.select("svg").append("path")
+                    .attr("d", "M129.6 36.6c5.6 0 10.3 1 13.2-4.2")
+                    .attr("class", "hair-shaggy-outline");
+  }
+  hairShaggy();
 
 
   window.legs = function(){
