a549eab574c732692145e33908f7275b3842c81b
Anh La Russo <Aphuong85@gmail.com>
2014-12-01 14:49:45 -0500

    Add Obscenity gem to filter profanity

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,4 +1,7 @@
 class Fruit < ActiveRecord::Base
   has_many :parts, through: :fruit_parts
   has_many :fruit_parts
+
+  validates :name, obscenity: { sanitize: true, replacement: "I love fruits" }
+
 end
